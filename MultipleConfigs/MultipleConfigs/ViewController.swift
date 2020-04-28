218f61f59588e153c221b4894ea27e63a8ab1d45
Manish Dhameja <manish20290@gmail.com>
2020-04-28 20:34:36 +0530

    Init

---
+++ MultipleConfigs/MultipleConfigs/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MultipleConfigs
+//
+//  Created by Manish on 28/04/20.
+//  Copyright © 2020 manish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
